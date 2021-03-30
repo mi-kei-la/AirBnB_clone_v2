3003558646bd802d663274f50c734f75f8d9411e
mi-kei-la <micaela.percovich@gmail.com>
2021-03-30 16:06:45 +0000

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from os import getenv
 from models.base_model import Base
+from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from models.user import User
@@ -22,12 +23,12 @@ class DBStorage:
         """
         # dialect+driver://username:password@host:port/database
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:{}'
-                                      .format(getenv(HBNB_MYSQL_USER),
-                                              getenv(HBNB_MYSQL_PWD),
-                                              getenv(HBNB_MYSQL_HOST),
-                                              getenv(HBNB_MYSQL_DB)),
+                                      .format(getenv("HBNB_MYSQL_USER"),
+                                              getenv("HBNB_MYSQL_PWD"),
+                                              getenv("HBNB_MYSQL_HOST"),
+                                              getenv("HBNB_MYSQL_DB")),
                                       pool_pre_ping=True)
-        if getenv(HBNB_ENV) is "test":
+        if getenv("HBNB_ENV") is "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
@@ -49,3 +50,27 @@ class DBStorage:
                     key = type(row).__name__ + '.' + row.id
                     objects[key] = row
         return objects
+
+    def new(self, obj):
+        """Add object to current database session.
+        """
+        self.__session.add(obj)
+
+    def save(self):
+        """Commit changes to current database session.
+        """
+        self.__session.commit()
+
+    def delete(self, obj):
+        """Delete object from the current database session.
+        """
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Create the current database session.
+        """
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
