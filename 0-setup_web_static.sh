7abe76815da57f01aef27bf9191208ecc7d11bb0
mi-kei-la <micaela.percovich@gmail.com>
2021-05-01 14:59:02 +0000

    aaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,12 +5,11 @@ apt-get -y update
 apt-get -y install nginx
 # Allow NginX through the firewall
 ufw allow 'Nginx HTTP'
+# Listen to port 80
+
 # Create folders if not existing
-mkdir -p /data
-mkdir -p /data/web_static
-mkdir -p /data/web_static/releases
-mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 # Create fake HTML file to test.
 touch /data/web_static/releases/test/index.html
 echo "<html>
@@ -26,7 +25,8 @@ ln -fs /data/web_static/releases/test/ /data/web_static/current
 # Give ownership (recursively) of the data/ folder to the ubuntu user AND group
 chown -R ubuntu:ubuntu /data/
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-new_content="\\ \tlocation = /hbnb_static {\n\t\t alias /data/web_static/current/;\n\tautoindex on;\n\t}"
-sed -i "/server_name _;/a $new_content" /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server/a location /hbnb_static {\n\talias /data/web_static/current/;\n\tautoindex on;\n}\n' /etc/nginx/sites-available/default
 # Restart NginX
 service nginx restart
+# Always exit successfully
+exit 0
