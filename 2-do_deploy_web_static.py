6f1077c05222f0994957fcdd12a26cb021a03dd7
mi-kei-la <micaela.percovich@gmail.com>
2021-05-03 01:15:17 +0000

    actual debugging

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,24 +10,25 @@ env.hosts = ['35.231.30.18', '34.73.105.29']
 def do_deploy(archive_path):
     """This function distributes an archive to your web servers"""
     if path.exists(archive_path) is False:
+        print('file not found')
         return False
 
     try:
         # Remove folder name and extension from file name
         slash = archive_path.find('/') + 1
-        filename = archive_path[find:]
+        filename = archive_path[slash:]
 
         # Upload archive to /tmp/
         put(archive_path, '/tmp/{}'.format(filename))
 
         # Uncompress file and delete original
         run('mkdir -p /data/web_static/releases/' + filename[:-4])
-        run('tar -xzvf /tmp/' + filename + '-C /data/web_static/releases/'
+        run('tar -xzvf /tmp/' + filename + ' -C /data/web_static/releases/'
             + filename[:-4] + '/')
         run('rm /tmp/' + filename)
 
         # Move uncompressed files to parent folder (file is a folder)
-        run('mv /data/web_static/releases' + filename[:-4] + '/web_static/* \
+        run('mv /data/web_static/releases/' + filename[:-4] + '/web_static/* \
         /data/web_static/releases/' + filename[:-4] + '/')
         run('rm -rf /data/web_static/releases/' + filename[:-4]
             + '/web_static')
@@ -40,4 +41,6 @@ def do_deploy(archive_path):
         return True
 
     except Exception as e:
+        print('error:')
+        print(e)
         return False
